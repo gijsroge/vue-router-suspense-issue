b2f3b176eb453707fb717b3e894fdb8334a5f7b3
gijs <gijs.roge@intracto.com>
2021-10-25 13:28:40 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view v-slot="{ Component }">
+    <template v-if="Component">
+        <keep-alive>
+          <suspense>
+            <component :is="Component"></component>
+            <template #fallback>
+              <div>
+                Loading...
+              </div>
+            </template>
+          </suspense>
+        </keep-alive>
+    </template>
+  </router-view>
+</template>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>About Page</h1>
+</template>
+
+<script setup>
+await new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(true);
+  }, 5000);
+});
+</script>
